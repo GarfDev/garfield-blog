841a3ad1b3f936d43215ae17ada09791f120af78
Nguyen Gia Tuan <garfdev.13@gmail.com>
2024-08-29 12:42:18 +0700

    feat: add style for markdown renderer

--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -1,13 +1,15 @@
+import { Link, type PageProps } from "gatsby";
 import React from 'react';
 import Markdown from 'react-markdown';
-import { Link, type PageProps } from "gatsby"
-import Editor from '../components/Editor';
-import Layout from '../components/Layout';
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { useRecoilValue } from 'recoil';
-import gistSelector from '../atoms/gist.selector';
+import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis'
+import rehypeClassNames from 'rehype-class-names';
 import rehypeHighlight from 'rehype-highlight';
-import rehypeClassNames from 'rehype-class-names'
 import remarkGfm from 'remark-gfm';
+import gistSelector from '../atoms/gist.selector';
+import Layout from '../components/Layout';
+import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism'
 
 const markdownClassname = {
   '*': 'my-2',
@@ -18,7 +20,7 @@ const markdownClassname = {
   'h5': 'text-2xl font-bold title-gradient',
   'h6': 'text-xl font-bold title-gradient',
   'a': 'visited:text-gray-500 font-bold cursor-pointer hover:text-gray-300 duration-100',
-  'pre': 'p-2 bg-gray-700',
+  'pre': 'p-2 !bg-gray-700 [&_pre]:!bg-gray-700 [&_pre]:!font-bold',
   'hr': 'title-gradient'
 }
 
@@ -32,12 +34,33 @@ const IndexRoute = ({ path, params }: PageProps) => {
     <Layout>
       <main className='min-h-[100vh] '>
         <div className='w-full min-h-[300px]'>
-          <Markdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeHighlight, [rehypeClassNames, markdownClassname]]}>
+          <Markdown
+            remarkPlugins={[remarkGfm]}
+            rehypePlugins={[rehypeHighlight, rehypeAccessibleEmojis, [rehypeClassNames, markdownClassname]]}
+            components={{
+              code({ node, className, children, ref, ...rest }) {
+                const match = /language-(\w+)/.exec(className || "");
+                return (
+                  match ? (
+                    <SyntaxHighlighter
+                      children={String(children).replace(/\n$/, "")}
+                      language={match?.[1] || undefined}
+                      style={{...dracula}}
+                    />
+                  )
+                    : (
+                      <code {...rest} className={className}>
+                        {children}
+                      </code>
+                    )
+                )
+              }
+            }}>
             {`${gist.files[Object.keys(gist.files)[0]].content}`}
           </Markdown>
         </div>
       </main>
-    </Layout>
+    </Layout >
   )
 }
 
